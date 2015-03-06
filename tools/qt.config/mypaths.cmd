0efc563a2da7f0b8484294382e09a506b2576d9a
Trevor Stavropoulos <Trevor.Stavropoulos@yale.edu>
2015-03-06 11:45:59 -0500

    Further generalization of the build process for 32 and 64 bit builds.

--- tools/qt.config/mypaths.cmd
+++ tools/qt.config/mypaths.cmd
@@ -1,11 +1,33 @@
+@REM The PATH variable contains parens, so we cannot have it nested in a set, or it crashes
+@REM the execution of the script.
+@IF NOT DEFINED PICTO_ENV_SET (
+  @echo Picto environment hasn't been configured
+  @echo Run the appropriate configure script for your platform from the Picto root directory and try again
+  EXIT /B
+)
+
+@REM Setting the directories for each of the supported builds
+@IF "%PICTO_MACHINE_TYPE%"=="X86" (
+  @set PICTO_QT_INSTALL_TOP_DIR=Qt
+  @set PICTO_QT_MSVC_DIR=msvc2013
+  @set PICTO_QWT_DIR=qwt
+)
+
+@IF "%PICTO_MACHINE_TYPE%"=="X64" (
+  @set PICTO_QT_INSTALL_TOP_DIR=Qt64
+  @set PICTO_QT_MSVC_DIR=msvc2013_64
+  @set PICTO_QWT_DIR=qwt_64
+)
+
 set INCLUDE=%PICTO_THIRD_PARTY%\openssl\inc32;%INCLUDE%
 set LIB=%PICTO_THIRD_PARTY%\openssl\lib;%LIB%
-set QTTOP=%PICTO_THIRD_PARTY%\Qt\Qt5.3.2\5.3
-set PATH=%PICTO_THIRD_PARTY%\openssl\bin;%QTTOP%\msvc2013\bin;%PATH%
-set QTDIR=%QTTOP%\msvc2013
+set QTTOP=%PICTO_THIRD_PARTY%\%PICTO_QT_INSTALL_TOP_DIR%\Qt5.3.2\5.3
+set PATH=%PICTO_THIRD_PARTY%\openssl\bin;%QTTOP%\%PICTO_QT_MSVC_DIR%\bin;%PATH%
+set QTDIR=%QTTOP%\%PICTO_QT_MSVC_DIR%
 set PROPBROWSDIR=%QTTOP%\Src\qttools\src\shared\qtpropertybrowser
-set QWTDIR=%PICTO_THIRD_PARTY%\qwt
+set QWTDIR=%PICTO_THIRD_PARTY%\%PICTO_QWT_DIR%
 set PHIDGETSDIR=%PICTO_THIRD_PARTY%\phidgets
 set OPENSSLDIR=%PICTO_THIRD_PARTY%\openssl
 @echo If desired, run the dx_setenv.cmd script from the Microsoft DirectX SDK
 @echo Note that the official build does not compile Qt with DirectX enabled
+
