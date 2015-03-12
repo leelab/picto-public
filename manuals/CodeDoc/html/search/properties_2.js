cb0c2f96d25f114615f8dda25efc8d1d9f964517
Trevor Stavropoulos <Trevor.Stavropoulos@yale.edu>
2015-03-12 15:13:46 -0400

    Updated documentation regarding the Reward Channel Change

--- manuals/CodeDoc/html/search/properties_2.js
+++ manuals/CodeDoc/html/search/properties_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['chan',['chan',['../class_picto_1_1_reward.html#a0c7824ab2fd020beb9827e7047daf88d',1,'Picto::Reward']]],
   ['color',['color',['../struct_picto_1_1_visual_element.html#a45c3042c969717eb2ab60373369c7de7',1,'Picto::VisualElement']]],
   ['columns',['columns',['../class_picto_1_1_grid_graphic.html#ab4519f754954e50d06711fbc2ba4608f',1,'Picto::GridGraphic']]]
 ];
