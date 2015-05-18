21b628d0a81cca5daa4a613ef0c44c04af31287b
Trevor Stavropoulos <Trevor.Stavropoulos@yale.edu>
2015-05-18 16:04:02 -0400

    Updating Documentation.

--- manuals/CodeDoc/html/search/classes_f.js
+++ manuals/CodeDoc/html/search/classes_f.js
@@ -67,5 +67,6 @@ var searchData=
   ['proxyresumeresponsehandler',['ProxyResumeResponseHandler',['../struct_proxy_resume_response_handler.html',1,'']]],
   ['proxystatusmanager',['ProxyStatusManager',['../class_proxy_status_manager.html',1,'']]],
   ['pseudorandomintparameter',['PseudorandomIntParameter',['../class_picto_1_1_pseudorandom_int_parameter.html',1,'Picto']]],
+  ['psthplot',['PSTHPlot',['../class_picto_1_1_p_s_t_h_plot.html',1,'Picto']]],
   ['putdatacommandhandler',['PutDataCommandHandler',['../struct_put_data_command_handler.html',1,'']]]
 ];
