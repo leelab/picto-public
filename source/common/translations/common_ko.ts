4e5ef11e6081ef76e2bd75fb97c62b889049ce7c
Michael Scudder <michael.scudder@yale.edu>
2019-08-16 15:16:16 -0400

    Restore translations files which are not automatically generated and add missing resource files for the Intan RHD2000.qrc

---
+++ source/common/translations/common_ko.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ko">
+<context>
+    <name>TranslatedNames</name>
+    <message>
+        <location filename="../namedefs.cpp" line="7"/>
+        <source>PictoServer</source>
+        <translation>상형문자서버</translation>
+    </message>
+    <message>
+        <location filename="../namedefs.cpp" line="8"/>
+        <source>PictoConfig</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../namedefs.cpp" line="9"/>
+        <source>PictoDirector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../namedefs.cpp" line="10"/>
+        <source>PictoBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../namedefs.cpp" line="11"/>
+        <source>PictoLab</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
